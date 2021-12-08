8f114779fc88f152a35557f957cb75dea5a4f515
Calmes <77333391+ChristopherKerhman@users.noreply.github.com>
2021-12-08 14:49:22 +0100

    Div1

--- formulaires/connexion.php
+++ formulaires/connexion.php
@@ -1,13 +1,9 @@
 <form class="formulaire" action="securite/connexion.php" method="post">
   <div class="colonne boxStandard">
-      <div class="colonne">
   <label for="speudo">Votre speudo</label>
   <input id="speudo" class="inputFormulaire" type="text" name="login" placeholder="Votre speudo" required>
-  </div>
-    <div class="colonne">
   <label for="mdp">Votre mot de passe</label>
   <input id="speudo" class="inputFormulaire" type="password" name="motDePasse" placeholder="Votre mot de passe" required>
-</div>
   <button class="buttonStandard" type="submit" name="button">Connexion</button>
 </div>
 </form>
