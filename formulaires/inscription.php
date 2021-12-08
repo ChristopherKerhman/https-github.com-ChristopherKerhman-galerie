6a89258069328f478df74d574f06bae5666f519b
Calmes <77333391+ChristopherKerhman@users.noreply.github.com>
2021-12-08 14:51:00 +0100

    div2

--- formulaires/inscription.php
+++ formulaires/inscription.php
@@ -2,22 +2,14 @@
     <div class="colonne boxStandard">
       <h3>Formulaire d'inscription</h3>
       <h4>Votre identité</h4>
-    <div class="colonne">
       <label for="nom">Nom</label>
       <input id="nom" class="inputFormulaire" type="text" name="nom">
-    </div>
-    <div class="colonne">
       <label for="prenom">Prenom</label>
       <input id="prenom" class="inputFormulaire" type="text" name="prenom">
-    </div>
-    <div class="colonne">
       <label for="login">Login</label>
       <input id="login" class="inputFormulaire" type="text" name="login">
-    </div>
-    <div class="colonne">
       <label for="mdp">mot de passe</label>
       <input id="mdp" class="inputFormulaire" type="text" name="mdp">
-    </div>
     <button class="buttonStandard" type="submit" name="button">Créer un compte</button>
     </div>
 </form>
